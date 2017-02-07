ac787ca9c45a036ed4e495ec11076c952b6f1bcf
Yida Wang <yidaalex.wang@gmail.com>
2017-02-06 22:28:48 -0500

    update RSI single stock

--- RSI_SingleStock.m
+++ RSI_SingleStock.m
@@ -1,55 +1,53 @@
-clear all;
+clear;
 close all;
 
-%% Initualize Variable
-%define moving intervel
+%% Initialize Variable
+%define RSI intervel
 Interval=10;
-%initual money 
-Initual_Value=3000000; 
-%% Import intual data
 
-Data_2015=xlsread('SP500_2015.csv',1,'G:G');
-Data_2015=fliplr(Data_2015);
-%Alibaba
-BaBa_2015=xlsread('BaBa_2015.csv',1,'G:G');
-BaBa_2015=fliplr(BaBa_2015);%reverse order start from day one
+%% Import intial data
+
 %IBM
-IBM_2015=xlsread('IBM_2015.csv',1,'G:G');
-IBM_2015=fliplr(IBM_2015);%reverse order start from day one
-%Tesla
-TSLA_2015=xlsread('TSLA_2015.csv',1,'G:G');
-TSLA_2015=fliplr(TSLA_2015);%reverse order start from day one
+IBM_2016=xlsread('IBM_2016.csv',1,'G:G');
+IBM_2016=fliplr(IBM_2016);%reverse order start from day one
 
 %% RSI
-Day_count=numel(BaBa_2015);
+Day_count=numel(IBM_2016);
 Period=floor(Day_count/Interval);
-RSI_BaBa=zeros(Period,1);
-RSI_IBM=zeros(Period,1);
-RSI_TSLA=zeros(Period,1);
+RSI_IBM=zeros(Day_count,1);
 
-temp_BaBa=zeros(Interval,1);
-temp_IBM=zeros(Interval,1);
-temp_TLSA=zeros(Interval,1);
+temp_IBM=zeros(Day_count,1);
+buy_dates = [];
+buy_prices = [];
+sell_dates = [];
+sell_prices = [];
 
-for count=1:Interval:Period*Interval
-    temp_BaBa=BaBa_2015(count:count+Interval-1);
-    temp_IBM=IBM_2015(count:count+Interval-1);
-    temp_TSLA=TSLA_2015(count:count+Interval-1);
-    RSI_BaBa((count-1)/Interval+1)=RSI(temp_BaBa);
-    RSI_IBM((count-1)/Interval+1)=RSI(temp_IBM);
-    RSI_TSLA((count-1)/Interval+1)=RSI(temp_TSLA);
+for count=Interval+1:Day_count
+    temp_IBM=IBM_2016(count-Interval:count);
+    RSI_IBM(count)=RSI(temp_IBM);
+    if count > 1 && RSI_IBM(count-1) > 30 && RSI_IBM(count) <= 30
+        buy_dates = [buy_dates count];
+        buy_prices = [buy_prices IBM_2016(count)];
+    elseif count > 1 && RSI_IBM(count-1) < 70 && RSI_IBM(count) >= 70
+        sell_dates = [sell_dates count];
+        sell_prices = [sell_prices IBM_2016(count)];
+    end
 end
-
 
 %% plot
 
-figure 
-plot(RSI_BaBa)
-hold on
+figure(1)
+plot(1:Day_count, IBM_2016, 'b', ...
+    buy_dates, buy_prices, 'g*', ...
+    sell_dates, sell_prices, 'r*');
+legend('IBM price', 'buy signal', 'sell signal');
+xlim([0, Day_count]);
+xlabel('Time')
+ylabel('Price')
+
+figure(2)
 plot(RSI_IBM)
-hold on
-plot(RSI_TSLA)
-hold off
-legend('show','Alibaba','IBM','TSLA')
-xlabel('Period')
+legend('show','IBM RSI')
+xlim([Interval, Day_count]);
+xlabel('Time')
 ylabel('RSI Value')
