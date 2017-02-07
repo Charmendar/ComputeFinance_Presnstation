004f0710ac060be36f8b447ffcc5d2ac41fd1e98
Yida Wang <yidaalex.wang@gmail.com>
2017-02-06 22:40:17 -0500

    update RSI

--- RSI_SingleStock.m
+++ RSI_SingleStock.m
@@ -19,8 +19,10 @@ RSI_IBM=zeros(Day_count,1);
 temp_IBM=zeros(Day_count,1);
 buy_dates = [];
 buy_prices = [];
+buy_rsi = [];
 sell_dates = [];
 sell_prices = [];
+sell_rsi = [];
 
 for count=Interval+1:Day_count
     temp_IBM=IBM_2016(count-Interval:count);
@@ -28,9 +30,11 @@ for count=Interval+1:Day_count
     if count > 1 && RSI_IBM(count-1) > 30 && RSI_IBM(count) <= 30
         buy_dates = [buy_dates count];
         buy_prices = [buy_prices IBM_2016(count)];
+        buy_rsi = [buy_rsi RSI_IBM(count)];
     elseif count > 1 && RSI_IBM(count-1) < 70 && RSI_IBM(count) >= 70
         sell_dates = [sell_dates count];
         sell_prices = [sell_prices IBM_2016(count)];
+        sell_rsi = [sell_rsi RSI_IBM(count)];
     end
 end
 
@@ -40,14 +44,16 @@ figure(1)
 plot(1:Day_count, IBM_2016, 'b', ...
     buy_dates, buy_prices, 'g*', ...
     sell_dates, sell_prices, 'r*');
-legend('IBM price', 'buy signal', 'sell signal');
+legend('IBM price', 'buy', 'sell');
 xlim([0, Day_count]);
 xlabel('Time')
 ylabel('Price')
 
 figure(2)
-plot(RSI_IBM)
-legend('show','IBM RSI')
+plot(1:Day_count, RSI_IBM, 'b', ...
+    buy_dates, buy_rsi, 'g*', ...
+    sell_dates, sell_rsi, 'r*')
+legend('show','IBM RSI', 'buy', 'sell')
 xlim([Interval, Day_count]);
 xlabel('Time')
 ylabel('RSI Value')
